c7ad97adf2a62229f090fdb475fda844c6991d92
unknown <valeria_linares1@usmp.pe>
2023-03-24 23:18:07 -0500

    Practica Calificada1

---
+++ Controllers/CobroController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaCalificada1.Models;
+
+namespace PracticaCalificada1.Controllers
+{
+    public class CobroController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(new Cobro());
+        }
+        [HttpPost]
+        public IActionResult Calcular(Cobro obj)
+        {
+            DateTime hoy = DateTime.Now;
+            int diferencia = (hoy - obj.fechaVenc).Days;
+            double mora = 0 , montoFinal;
+
+            if (diferencia > 0)
+            {
+                mora = diferencia * 0.0005 * obj.montoPagar;
+            }
+            montoFinal = obj.montoPagar + mora;
+
+            ViewData["Message"] = "El Pago final es de " + montoFinal;
+
+            return View("Index");
+        }
+    }
+}
---
+++ Models/Cobro.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticaCalificada1.Models
+{
+    public class Cobro
+    {
+        public string numTarjeta { get; set; }
+        public DateTime fechaVenc { get; set; }
+        public double montoPagar { get; set; }
+    }
+}
